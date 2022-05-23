74abaf2979fbdf46c2c75fcb6a8f6e759ca5917e
Vesoboy <103992650+Vesoboy@users.noreply.github.com>
2022-05-23 19:43:17 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Infomation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    internal class Infomation
+    {
+        public void Info()
+        {
+                    MessageBox.Show(
+                    "Подумай что ты делаешь! Ты же не дурак", "Ошибка", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+        }
+        public void InfoDel()
+        {
+            MessageBox.Show(
+            "Ооо боже, не дели на ноль, \n а то я тебя на ноль поделю!", "Грубая ошибка",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information,
+            MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+    }
+}
